9a9be1c663765955818717f14e758a0b8b8fa381
Ryan Poon <rypoon18@gmail.com>
2024-09-20 09:51:31 -0400

    send requests to followers concurrently

--- src/main/java/com/rynamo/ring/coordinate/Coordinator.java
+++ src/main/java/com/rynamo/ring/coordinate/Coordinator.java
@@ -11,6 +11,10 @@ import com.rynamo.ring.entry.RingEntry;
 import io.grpc.StatusRuntimeException;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.concurrent.ForkJoinTask.invokeAll;
 
 public class Coordinator {
     private final Node node;
@@ -22,42 +26,106 @@ public class Coordinator {
         System.out.println(key);
         List<RingEntry> preferenceList = this.node.getPreferenceList(key);
         List<byte[]> results = new ArrayList<>();
+        List<Callable<List<ByteString>>> tasks = calculateGetTasks(key, preferenceList);
+
+        int cores = Runtime.getRuntime().availableProcessors();
         int reads = 0;
-        for (int i = 0; i < preferenceList.size() && reads < this.node.R; i++) {
-            RingEntry entry = preferenceList.get(i);
-            if (entry instanceof ActiveEntry active) {
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(cores);
+        try {
+            List<Future<List<ByteString>>> taskResults = taskExecutor.invokeAll(tasks);
+            for (Future<List<ByteString>> future : taskResults) {
                 try {
-                    GetResponse response = active.get(key);
-                    for (ByteString value : response.getValueList()) {
-                        results.add(value.toByteArray());
+                    List<ByteString> oneTaskResults = future.get();
+                    if (!oneTaskResults.isEmpty()) {
+                        for (ByteString value : oneTaskResults) {
+                            results.add(value.toByteArray());
+                        }
+                        reads++;
                     }
-                    reads++;
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (CancellationException | ExecutionException ce) {
+                    ce.printStackTrace();
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt(); // ignore/reset
                 }
             }
+            return new CoordinateResponse(reads, 0, results);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return new CoordinateResponse(reads, 0, results);
         }
-        return new CoordinateResponse(reads, 0, results);
+    }
+
+    private List<Callable<List<ByteString>>> calculateGetTasks(String key, List<RingEntry> preferenceList) {
+        List<Callable<List<ByteString>>> tasks = new ArrayList<>();
+        int activeNodesTried = 0;
+        for (int i = 0; i < preferenceList.size() && activeNodesTried < this.node.N; i++) {
+            RingEntry entry = preferenceList.get(i);
+            if (entry instanceof ActiveEntry active) {
+                activeNodesTried++;
+                tasks.add(() -> {
+                    try {
+                        GetResponse response = active.get(key);
+                        return response.getValueList();
+                    } catch (StatusRuntimeException e) {
+                        e.printStackTrace();
+                        return new ArrayList<>() {};
+                    }
+                });
+            }
+        }
+        return tasks;
     }
 
     public CoordinateResponse coordinatePut(String key, long version, byte[] val) {
         List<RingEntry> preferenceList = this.node.getPreferenceList(key);
-        System.out.println(preferenceList);
+        List<Callable<Long>> tasks = this.calculatePutTasks(key, version, val, preferenceList);
+
+        int cores = Runtime.getRuntime().availableProcessors();
         int writes = 0;
-        for (int i = 0; i < preferenceList.size() && writes < this.node.W; i++) {
-            RingEntry entry = preferenceList.get(i);
-            if (entry instanceof ActiveEntry active) {
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(cores);
+
+        try {
+            List<Future<Long>> taskResults = taskExecutor.invokeAll(tasks);
+            for (Future<Long> future : taskResults) {
                 try {
-                    PutResponse response = active.put(key, version, val);
-                    writes++;
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    Long oneTaskResults = future.get();
+                    if (oneTaskResults > -1) {
+                        writes++;
+                    }
+                } catch (CancellationException | ExecutionException ce) {
+                    ce.printStackTrace();
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt(); // ignore/reset
                 }
             }
-        }
-        if (writes >= this.node.W) {
-            this.node.db().setVersion(key, version + 1);
+            if (writes>= this.node.W) {
+                this.node.db().setVersion(key, version + 1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return new CoordinateResponse(0, writes, null);
+    }
+
+    private List<Callable<Long>> calculatePutTasks(String key, long version, byte[] val,
+                                                               List<RingEntry> preferenceList) {
+        List<Callable<Long>> tasks = new ArrayList<>();
+        int activeNodesTried = 0;
+        for (int i = 0; i < preferenceList.size() && activeNodesTried < this.node.N; i++) {
+            RingEntry entry = preferenceList.get(i);
+            if (entry instanceof ActiveEntry active) {
+                activeNodesTried++;
+                tasks.add(() -> {
+                    try {
+                        PutResponse response = active.put(key, version, val);
+                        return response.getVersion();
+                    } catch (StatusRuntimeException e) {
+                        e.printStackTrace();
+                        return (long) -1;
+                    }
+                });
+            }
+        }
+        return tasks;
     }
 }
